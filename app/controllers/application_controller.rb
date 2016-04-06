b9873268f197aefff3f554e210198a51d4037627
vadimkuzn <vadya@starlink.ru>
2016-04-06 19:46:05 +0300

    some minor changes related to URI analysis

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+ # Prevent CSRF attacks by raising an exception.
+ # For APIs, you may want to use :null_session instead.
+
+ String.class_eval do
+  def is_valid_url?
+   uri = URI.parse self
+   uri.kind_of? URI::HTTP
+    rescue URI::InvalidURIError
+   false
+  end
+ end
+
+ protect_from_forgery with: :exception
 end
